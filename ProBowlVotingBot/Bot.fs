3ee7d5dc2ebbe941b93fdfd3468d41b81c233df4
mrbandler <baudler.michael@gmail.com>
2018-11-30 16:17:46 +0100

    Created initial release

--- ProBowlVotingBot/Bot.fs
+++ ProBowlVotingBot/Bot.fs
@@ -64,7 +64,7 @@ module Bot =
         let page = 
             browser
             |> newPage
-            |> goTo VotingWebsite
+            |> goTo VotingWebsite   
             |> waitForSelector positionSelector
             |> click positionSelector
         
